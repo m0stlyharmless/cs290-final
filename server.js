4ae35e070144ffb13c4d434c01de4399db5b80ce
Michael Burton <burtomic@oregonstate.edu>
2018-12-01 12:32:22 -0800

    Server setup with basic handlebars template

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var exphbs = require('express-handlebars');
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.status(200).render('homePage', {
+
+	});
+});
+
+app.listen(port, function() {
+	console.log("Server is on port", port);
+});

501dfcfe159e3c7e4789fadf597b3fa62203d476
Ethan Schaezler <ethan26226@gmail.com>
2018-12-02 17:33:09 -0800

    Add axios

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const path = require('path');
 const app = express();
 const exphbs = require('express-handlebars');
 const port = process.env.PORT || 3000;
+const axios = require('axios');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+
+
 
 app.get('/teacher-hub', function(req, res) {
 	res.status(200).render('teacherHub', {

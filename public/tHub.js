33b887eb5021e3e26d4017429b5bddc34bee499b
Joshua Wentzel <wentzelj@flip1.engr.oregonstate.edu>
2018-12-04 15:47:56 -0800

    implemented json requesting

--- public/tHub.js
+++ public/tHub.js
@@ -1,14 +1,13 @@
 var inputField = document.getElementById('id-text');
 var createButton = document.getElementById('create-quiz');
 var editButton = document.getElementById('edit-quiz');
-var quizData = require('./quizData');
 
 createButton.addEventListener('click', createButtonPressed);
 editButton.addEventListener('click', editButtonPressed);
 
-sURL = window.location.protocol + '//' + window.location.host + '/student-hub';
-tURL = window.location.protocol + '//' + window.location.host + '/teacher-hub';
-
+//sURL = window.location.protocol + '//' + window.location.host + '/student-hub';
+//tURL = window.location.protocol + '//' + window.location.host + '/teacher-hub';
+console.log(5 + 6);
 
 function createButtonPressed() {
 	
@@ -18,12 +17,65 @@ function createButtonPressed() {
 
 function editButtonPressed() {
 	quiz = inputField.value;
-	if (quizData[quiz]) {
-		
-	}
-	else
-	{
-		alert("This quiz ID doesn't exist yet. Consider creating this quiz.");
-	}
+	console.log("QuizRequested = <" + quiz + ">");
+	//console.log(loadJSON());
+	quizData = {};
+	 //JSON.parse(loadJSON("quizData"));
+	 
+	 loadJSON(function(response) {
+	  // Parse JSON string into object
+		var actual_JSON = JSON.parse(response);
+		quizData = actual_JSON;
+		console.log(quizData);
+		if (quizData["123"]) {
+			alert("Passed");
+		}
+		else
+		{
+			alert("This quiz ID doesn't exist yet. Consider creating this quiz.");
+		}
+	 });
+	 
+	//quizData = loadJSON();
+	console.log(quizData);
+	
 	//window.location.href = tURL;
 }
+
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'quizData.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+	  //console.log ("Response: " + xobj.responseText);
+	  //console.log(JSON.parse(xobj.responseText));
+      //return JSON.parse(xobj.responseText);
+	  callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);  
+}
+/*
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ */
+ /*
+ function init() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+ });
+}
+*/
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const exphbs = require('express-handlebars');
 const port = process.env.PORT || 3000;
+var fs = require('fs'); /* Put it where other modules included */
+var data = JSON.parse(fs.readFileSync('./quizData.json', 'utf8')); /* Inside the get function */
+
+//var quizData = require('./quizData');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -10,23 +14,46 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/teacher-hub', function(req, res) {
-	res.status(200).render('teacherHub', {
-		
-	});
+	res.status(200).render('teacherHub', {});
 
 });
 
 app.get('/student-hub', function(req, res) {
-	res.status(200).render('studentHub', {
-		
-	});
+	res.status(200).render('studentHub', {});
 
 });
 
-app.get('/', function(req, res) {
-	res.status(200).render('homePage', {
+app.get('/quizData', function(req, res) {
+	res.status(200).render('studentHub', {});
 
-	});
+});
+
+app.get('/quizData.json', (req, res) => {
+  //console.log(res)
+   console.log("Requested quizData.json");
+
+  /* Insted of doing all this */
+  // res.writeHead(200, {
+  //    'Content-type': 'application/json'
+  // });
+  // res.end(JSON.stringify(data));
+
+  /* Just send the file */
+  res.sendFile(path.join(__dirname, 'quizData.json'));
+});
+/*
+
+app.set('/quizData.json', (req, res) => {
+  //console.log(res)
+   console.log("Requested quizData.json");
+
+
+
+  res.sendFile(path.join(__dirname, 'quizData.json'));
+});
+*/
+app.get('/', function(req, res) {
+	res.status(200).render('homePage', {});
 });
 
 app.listen(port, function() {

fac88ed0b640ac0cf7a4777e3344761fbbb12c8f
Joshua Wentzel <wentzelj@flip3.engr.oregonstate.edu>
2018-12-03 16:19:05 -0800

    moved tHub.js to correct location and cleaned up quizData.json

--- public/tHub.js
+++ public/tHub.js
@@ -1,6 +1,7 @@
 var inputField = document.getElementById('id-text');
 var createButton = document.getElementById('create-quiz');
 var editButton = document.getElementById('edit-quiz');
+var quizData = require('./quizData');
 
 createButton.addEventListener('click', createButtonPressed);
 editButton.addEventListener('click', editButtonPressed);
@@ -10,11 +11,19 @@ tURL = window.location.protocol + '//' + window.location.host + '/teacher-hub';
 
 
 function createButtonPressed() {
+	
 	//alert("This quiz ID is already in use. Consider editting this quiz.");
 	//window.location.href = sURL;
 }
 
 function editButtonPressed() {
-	//alert("This quiz ID doesn't exist yet. Consider creating this quiz.");
+	quiz = inputField.value;
+	if (quizData[quiz]) {
+		
+	}
+	else
+	{
+		alert("This quiz ID doesn't exist yet. Consider creating this quiz.");
+	}
 	//window.location.href = tURL;
 }

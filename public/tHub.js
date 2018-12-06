7c2a597799221ef38fa3a3a6a4a3aac76c53fff7
Joshua Wentzel <explojoe@yahoo.com>
2018-12-05 17:13:53 -0800

    implemented ID checking on create/edit buttons

--- public/tHub.js
+++ public/tHub.js
@@ -10,36 +10,39 @@ editButton.addEventListener('click', editButtonPressed);
 console.log(5 + 6);
 
 function createButtonPressed() {
-	
-	//alert("This quiz ID is already in use. Consider editting this quiz.");
-	//window.location.href = sURL;
+    quiz = inputField.value;
+    console.log("QuizRequested = <" + quiz + ">");
+    loadJSON(function(response) {
+        // Parse JSON string into object
+		var actual_JSON = JSON.parse(response);
+		quizData = actual_JSON;
+		console.log(quizData);
+		if (quizData[quiz]) {
+			alert("This quiz ID is already in use. Consider editting this quiz.");
+		}
+		else
+		{
+			// Passed
+		}
+	 });
 }
 
 function editButtonPressed() {
 	quiz = inputField.value;
 	console.log("QuizRequested = <" + quiz + ">");
-	//console.log(loadJSON());
-	quizData = {};
-	 //JSON.parse(loadJSON("quizData"));
-	 
-	 loadJSON(function(response) {
-	  // Parse JSON string into object
+    loadJSON(function(response) {
+        // Parse JSON string into object
 		var actual_JSON = JSON.parse(response);
 		quizData = actual_JSON;
 		console.log(quizData);
-		if (quizData["123"]) {
-			alert("Passed");
+		if (quizData[quiz]) {
+			// Passed
 		}
 		else
 		{
 			alert("This quiz ID doesn't exist yet. Consider creating this quiz.");
 		}
 	 });
-	 
-	//quizData = loadJSON();
-	console.log(quizData);
-	
-	//window.location.href = tURL;
 }
 
 function loadJSON(callback) {   
@@ -48,34 +51,8 @@ function loadJSON(callback) {
   xobj.open('GET', 'quizData.json', true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
-	  //console.log ("Response: " + xobj.responseText);
-	  //console.log(JSON.parse(xobj.responseText));
-      //return JSON.parse(xobj.responseText);
 	  callback(xobj.responseText);
     }
   };
   xobj.send(null);  
 }
-/*
-function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
- */
- /*
- function init() {
- loadJSON(function(response) {
-  // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
- });
-}
-*/

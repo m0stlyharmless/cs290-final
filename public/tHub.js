4d2022d7aa4c35add2e563a692bfa8d035699cfd
Joshua Wentzel <joshuahwentzel@gmail.com>
2018-12-05 17:05:17 -0800

    fixed json file readng

--- public/tHub.js
+++ public/tHub.js
@@ -27,7 +27,7 @@ function editButtonPressed() {
 		var actual_JSON = JSON.parse(response);
 		quizData = actual_JSON;
 		console.log(quizData);
-		if (quizData["123"]) {
+		if (quizData[quiz]) {
 			alert("Passed");
 		}
 		else
@@ -56,26 +56,3 @@ function loadJSON(callback) {
   };
   xobj.send(null);  
 }
-/*
-function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
- */
- /*
- function init() {
- loadJSON(function(response) {
-  // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
- });
-}
-*/
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const exphbs = require('express-handlebars');
 const port = process.env.PORT || 3000;
 var fs = require('fs'); /* Put it where other modules included */
-var data = JSON.parse(fs.readFileSync('./quizData.json', 'utf8')); /* Inside the get function */
+
 
 //var quizData = require('./quizData');
 
@@ -14,70 +14,53 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/teacher-hub', function(req, res) {
-	res.status(200).render('teacherHub', {});
+    res.status(200).render('teacherHub', {});
 
 });
 
 app.get('/student-hub', function(req, res) {
-	res.status(200).render('studentHub', {});
+    res.status(200).render('studentHub', {});
 
 });
 
 
 app.get('/quizData', function(req, res) {
-	res.status(200).render('studentHub', {});
+    res.status(200).render('studentHub', {});
 
 });
 
 app.get('/quizData.json', (req, res) => {
-  //console.log(res)
-   console.log("Requested quizData.json");
+    var data = JSON.parse(fs.readFileSync('./quizData.json', 'utf8'));
+    console.log("Requested quizData.json");
 
-  /* Insted of doing all this */
-  // res.writeHead(200, {
-  //    'Content-type': 'application/json'
-  // });
-  // res.end(JSON.stringify(data));
-
-  /* Just send the file */
-  res.sendFile(path.join(__dirname, 'quizData.json'));
+    res.sendFile(path.join(__dirname, 'quizData.json'));
 });
-/*
 
-app.set('/quizData.json', (req, res) => {
-  //console.log(res)
-   console.log("Requested quizData.json");
-
-
-
-  res.sendFile(path.join(__dirname, 'quizData.json'));
-});
-*/
 app.get('/', function(req, res) {
-	res.status(200).render('homePage', {});
+    res.status(200).render('homePage', {});
 });
 
 app.get('/quizzes/:id', function (req, res, next) {
-	var quizId = req.params.id.toLowerCase();
-	if (data[quizId]) {
-		res.render('quizzes', data[quizId]);
-	} else {
-		next();
-	}
+    var quizId = req.params.id.toLowerCase();
+    if (data[quizId]) {
+        res.render('quizzes', data[quizId]);
+    } else {
+        next();
+    }
 });
 
 /*app.get('/quizzes', function(req, res) {
-	res.status(200).render('testQuizzes', {
+    res.status(200).render('testQuizzes', {
 
-	});
+    });
 });*/
 
 app.get('*', function(req, res) {	
-	res.status(404).render('404', {
+    res.status(404).render('404', {
 
-	});
+    });
 });
 
 app.listen(port, function() {
-	console.log("Server is on port", port);
+    console.log("Server is on port", port);
 });
